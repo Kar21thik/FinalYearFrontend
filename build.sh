ae6dc85f4987e2e4626c3752bcd219e7c17b1b2e
Ganesh23Nayak <ganeshrnayak23@gmail.com>
2024-09-30 22:48:21 +0530

    fixed errors

--- build.sh
+++
@@ -1,8 +0,0 @@
-echo '#!/usr/bin/expect
-set timeout 20
-spawn ssh -p 202 -L 8080:localhost:5000 4nm21cs080@gpu.nmamit.in
-expect "password:"
-send "hehehehe\r"
-interact' > conn.exp
-chmod +x conn.exp
-./conn.exp
